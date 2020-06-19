fffb23e984d302754f1badefad4dc75af34e5a65
Arth Tyagi <arthtyagi7@gmail.com>
2020-06-19 21:47:35 +0530

    Update settings.py

--- geddit/settings.py
+++ geddit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-pbgbo34e_bkz&cr7no01chky*i1zj(_=e=gkl4aq7ylsbo!a9'
+SECRET_KEY = '--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
