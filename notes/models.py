0eb2384539436b582253300824502dddf774f7d0
Arth Tyagi <arthtyagi7@gmail.com>
2020-06-25 22:20:48 +0530

    labels can now be 25 character long

--- notes/models.py
+++ notes/models.py
@@ -4,14 +4,13 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 class Notes(models.Model):
     title = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    category = models.CharField(max_length =10, default = "CS")
+    category = models.CharField(max_length =25, default = "CS")
     slug = models.SlugField(max_length=250,unique_for_date='created', default = timezone.now)
     def __str__(self):
         return self.title
