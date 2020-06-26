3a9a96851729b9e8b8e2d0d6f7609edb79e540d0
Arth Tyagi <arthtyagi7@gmail.com>
2020-06-27 00:34:45 +0530

    updatedf

--- todo/models.py
+++ todo/models.py
@@ -6,13 +6,11 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
 	title = models.CharField(max_length=350)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	content = models.CharField(max_length = 550 )
+	content = models.CharField(max_length=550)
 	created = models.DateTimeField(auto_now_add=True)
-
-
 
 	def __str__(self):
 		return self.title
-		
+
 	def get_absolute_url(self):
 		return reverse('todo:detail', kwargs={'pk': self.pk})
