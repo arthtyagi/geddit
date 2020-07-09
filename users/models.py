aae50649623819aa1f76b9e6ade3be618deb73ad
Arth Tyagi <arthtyagi7@gmail.com>
2020-07-10 00:46:43 +0530

    added validation in Profile model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import validate_image_file_extension
 from django.contrib.auth.models import User
 from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics/', validators = [validate_image_file_extension])
 
     def __str__(self):
         return f'{self.user.username} Profile'
